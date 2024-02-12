53b5fdc01f7e448d7f7cbc6e24c2825bc45a8ac4
BrageDrage909 <bfu01.bb@gmail.com>
2024-02-12 14:11:42 +0100

    update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(staticPath));
 
 app.get("/getUserRoles", getUserRoles)
 
-app.get("/getUserWorkgroups", getUserWorkgroups)
+app.post("/checkCredentials", checkCredentials)
 
 async function getUserRoles(request, response) {
     const sql = db.prepare('SELECT * FROM usertype')
@@ -37,14 +37,58 @@ async function getUserRoles(request, response) {
     response.send(roles)
 }
 
-async function getUserWorkgroups (request, response)  {
-    const sql = db.prepare('SELECT * FROM workgroup')
-    let rows = sql.all()
-    let workgroups = rows.map(workgroup => ({
-        id: workgroup.ID,
-        name: workgroup.name
-    }));
-    response.send(workgroups)
+async function checkCredentials(request, response){
+    const user = request.body
+    const isAvailableEmail = await checkAvailability("users", "email", user.userCredentials.email);
+    if(isAvailableEmail === true){
+        if(user.userCredentials.workspaceAction === "1"){
+            const isGroupExist = await checkAvailability("workgroup", "groupCode", user.userCredentials.groupinfo)
+            if(isGroupExist === false){
+                createUser(user.userCredentials, "join")
+            } else{
+                console.log("There is no group with this code!")
+                response.send({ErrorMessage: "There is no group with this code!" })
+            }
+        } else if(user.userCredentials.workspaceAction === "2"){
+
+        }
+    } else {
+        console.log("A user is already created with that email!")
+        response.send({ErrorMessage: "A user is already created with that email!" })
+    }
+    };
+
+async function checkAvailability(table, type, info){
+    const validTables = ['users', 'workgroup'];
+    const validColumns = ['name', 'email', 'groupCode'];
+    if (validTables.includes(table) && validColumns.includes(type)) {
+        let isAvailable
+        const sql = db.prepare(`SELECT * FROM ${table} WHERE ${type} = ?`)
+        let check = sql.all(info)
+        if(check.length === 0){
+            isAvailable = true
+        } else {
+            isAvailable = false
+        }
+        return isAvailable
+    }
+}
+
+async function createUser(user, type){
+    if(type === "join"){
+        let sqlGetId = db.prepare("SELECT ID FROM workgroup WHERE groupCode = ?")
+        let groupId = sqlGetId.all(user.groupinfo)
+        console.log(groupId[0].ID)
+        const hashedPassword = await bcrypt.hash(user.password, 10)
+        const UUID = uuid.v4();
+        let sqlCreateUser = db.prepare("INSERT INTO users (uuid, name, email, password, usertype, workgroup, userStatus ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,)")
+        let createdUser = sqlCreateUser.all(UUID, user.name, user.email, hashedPassword, user.role, groupId[0].ID, false)
+    }
+}
+
+
+async function createGroup(){
+
 }
 
 app.listen(3012, () => {
